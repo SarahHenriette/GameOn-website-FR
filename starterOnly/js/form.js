f5f718d657afce7ffcfc4fa2514450971586a0ea
elise <rousseaujumelle@gmail.com>
2021-06-22 18:13:42 +0200

    refacto

--- starterOnly/js/form.js
+++ starterOnly/js/form.js
@@ -67,6 +67,16 @@ function error(element, messageError, smallError) {
         }
     })
 
+    birthdate.addEventListener("keyup", function(){
+        let msgErreur = birthdate.parentNode.childNodes[7]
+        if(birthdate.value.substr(0, 4) > 1900) {
+            success(birthdate, msgErreur)
+        }else {
+            error(birthdate, "Veuillez entrer une date de naissance valide.", msgErreur)
+
+        }
+    })
+
     quantity.addEventListener("keyup", function(){
         let msgErreur = quantity.parentNode.childNodes[6]
         if(quantity.value !== "" && isNaN(quantity.value) == false ) {
@@ -97,7 +107,7 @@ function error(element, messageError, smallError) {
         }
     })
     
-
+console.log(birthdate.value )
 function validate(event) {
     //vérifie si l'une des checkbox est checké ou pas
     checkbox.forEach(element => {
@@ -105,10 +115,13 @@ function validate(event) {
             checkedCheckbox = true
         }
     })
+    console.log(birthdate.value.substr(0, 4) )
+
     //si toutes les conditions sont remplis je valide
     if( regexMin.test(first.value) &&
         regexMin.test(last.value) &&
         regexEmail.test(email.value) &&
+        birthdate.value.substr(0, 4) > 1900 &&
         birthdate.value !== "" &&
         quantity.value !== "" &&
         isNaN(quantity.value) == false &&
@@ -116,42 +129,44 @@ function validate(event) {
         checkbox1.checked == true
         ){
             event.preventDefault()
-            //création div contenant msg avec les bouton close
-            let validationMessage = document.createElement('div')
-            let validationMsg = document.createElement('p')
-            let validationMessageClose = document.createElement('span')
-            let validationMessageBtnClose = document.createElement('button')
-            //attribution des class aux éléments
-            validationMessage.classList.add('msg')
-            validationMessageClose.classList.add('close')
-            validationMessageBtnClose.classList.add('btnClose')
-            
-            validationMsg.textContent= "Merci d'avoir soumis vos informations d'inscription"
-            validationMessageBtnClose.textContent = "Close"
-
-            body.appendChild(validationMessage)
-            validationMessage.appendChild(validationMessageClose)
-            validationMessage.appendChild(validationMessageBtnClose)
-            validationMessage.appendChild(validationMsg)
-
-
-            //fermeture du message de confirmation
-            validationMessageClose.addEventListener('click', function(){
-                validationMessage.style.display = "none"
-            })
-
-            validationMessageBtnClose.addEventListener('click', function(){
-                validationMessage.style.display = "none"
-            })
-
             //suppression du formulaire
             formulaire.style.display="none"
+
+            setTimeout(()=> {
+                //création div contenant msg avec les bouton close
+                let validationMessage = document.createElement('div')
+                let validationMsg = document.createElement('p')
+                let validationMessageClose = document.createElement('span')
+                let validationMessageBtnClose = document.createElement('button')
+                //attribution des class aux éléments
+                validationMessage.classList.add('msg')
+                validationMessageClose.classList.add('close')
+                validationMessageBtnClose.classList.add('btnClose')
+                
+                validationMsg.textContent= "Merci d'avoir soumis vos informations d'inscription"
+                validationMessageBtnClose.textContent = "Close"
+
+                body.appendChild(validationMessage)
+                validationMessage.appendChild(validationMessageClose)
+                validationMessage.appendChild(validationMessageBtnClose)
+                validationMessage.appendChild(validationMsg)
+
+                //fermeture du message de confirmation
+                validationMessageClose.addEventListener('click', function(){
+                    validationMessage.style.display = "none"
+                })
+
+                validationMessageBtnClose.addEventListener('click', function(){
+                    validationMessage.style.display = "none"
+                })
+            }, 500)
 
             //reset le formulaire
             form.reset()
             textControl.forEach(element => {
                 element.classList.remove("success")
             });
+
             //Je remet les checkbox a false
             checkedCheckbox = false
 
@@ -173,6 +188,10 @@ function validate(event) {
         if(birthdate.value == "") {
             let msgErreur = birthdate.parentNode.childNodes[7]
             error(birthdate, "Vous devez entrer votre date de naissance.", msgErreur)
+        } 
+        if(birthdate.value.substr(0, 4) > 1900) {
+            let msgErreur = birthdate.parentNode.childNodes[7]
+            error(birthdate, "Veuillez entrer une date de naissance valide.", msgErreur)
         }
         if(quantity.value == "") {
             let msgErreur = quantity.parentNode.childNodes[6]
